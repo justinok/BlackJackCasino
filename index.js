4700b5eadb297b517e4f1aba549244457a8d8584
justinok <38015681+justinok@users.noreply.github.com>
2022-07-14 14:37:30 -0500

    Added the display of cards, sum and button
    crate the function for the button of start game, added the function of showing the cards selected and its sum

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-let firstCard = 19;
-let secondCard = 9;
-let hasBlackJack = false;
-let isAlive = true;
-
-let sum = firstCard + secondCard;
-
-let messageEL = document.getElementById("message-el")
-console.log(messageEL)
-function startGame(){
-    if (sum < 21){
-        console.log("Do you want to draw a new card?")
-    }
-    if (sum === 21){
-        console.log("Congrats!! You've got BlackJack")
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEL = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEL.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
         hasBlackJack = true
-    }
-    if (sum > 21){
-        console.log("You lose")
+    } else {
+        message = "You're out of the game!"
         isAlive = false
     }
+    messageEl.textContent = message
 }
-
