69d867e73b954b3e1e8642e6dbc9a9a7c52984dc
justinok <38015681+justinok@users.noreply.github.com>
2022-07-14 23:47:20 -0500

    Images adition and changed the structure of the sum
    Adding the images to crate a better interface and help the player understand better the game, also added some methods on index.js in order to recive the images

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # BlackJackCasino
- Emulation of a black jack game using JavaScript played by console
+ Emulation of a black jack game using JavaScript.
+
+## Rules
+- Numeric cards have a value of $100
+- A, K, Q and J have a value of $500
+- If player gets Blackjack, you will also add $1000.
+- Every round has a cost of -$250
+
+## How to use the code
+Just click on code, you can dowload the ZIP or you can clone the repository
+When you have the code open you can use the ''' live server extension ''' to run the game.
+Also if you are on WebStorm you can use the HTML render. On both cases you only need to run
+the '''index.html''' file to run the game
+
+created by Justin Diaz
+
--- index.js
+++ index.js
@@ -14,8 +14,85 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+let extraMoney = 0
+
+
+window.onload = function() {
+    buildDeck();
+    shuffleDeck();
+}
 
 playerEl.textContent = player.name + ": $" + player.Accumulated
+
+var deck;
+
+
+window.onload = function() {
+    buildDeck();
+    shuffleDeck();
+}
+
+function buildDeck() {
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let types = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < types.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            deck.push(values[j] + "-" + types[i]); //A-C -> K-C, A-D -> K-D
+        }
+    }
+    // console.log(deck);
+}
+
+function shuffleDeck() {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length); // (0-1) * 52 => (0-51.9999)
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
+}
+
+
+function hit() {
+    if (!isAlive) {
+        return;
+    }
+
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + card + ".png";
+    sum += getValue(card);
+    document.getElementById("your-cards").append(cardImg);
+    renderGame()
+}
+
+
+
+function getValue(card) {
+    let data = card.split("-"); // "4-C" -> ["4", "C"]
+    let value = data[0];
+
+    if (isNaN(value)) { //A J Q K
+        if (value == "A") {
+            extraMoney += 500
+            return 11;
+        }
+        extraMoney += 500
+        return 10;
+    }
+    extraMoney += 100
+    return parseInt(value);
+}
+
+
+
+/**
+ * -----------------------------------
+ * @returns {number}
+ */
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
@@ -28,16 +105,21 @@ function getRandomCard() {
     }
 }
 
+
 function startGame() {
 
     if ( isInGame == false) {
         player.Accumulated -= 250
         isAlive = true
-        let firstCard = 10 //getRandomCard()
-        let secondCard = 11 //getRandomCard()
-        cards = [firstCard, secondCard]
-        sum = firstCard + secondCard
         isInGame = true
+        for (let i = 0; i < 2; i++) {
+            let cardImg = document.createElement("img");
+            let card = deck.pop();
+            cardImg.src = "./cards/" + card + ".png";
+            sum += getValue(card);
+            document.getElementById("your-cards").append(cardImg);
+        }
+
         renderGame()
     }
 }
@@ -45,9 +127,7 @@ function startGame() {
 function renderGame() {
 
     cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + SUITS[i]+ " "
-    }
+
 
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -55,7 +135,9 @@ function renderGame() {
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         player.Accumulated += 1000
+        player.Accumulated += extraMoney
         hasBlackJack = true
+        isAlive = false
     } else {
         message = "You're out of the game!"
         isAlive = false
@@ -75,7 +157,18 @@ function newCard() {
 }
 
 function newRound() {
+
     isInGame = false
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./cards/" + "BACK" + ".png";
+    document.getElementById("your-cards").append(cardImg);
+    sum = 0
+    extraMoney = 0
+
+    buildDeck();
+    shuffleDeck();
     startGame()
+
 
 }
