886976ee794938c21a62e2cf9a0c908a9b3ad105
justinok <38015681+justinok@users.noreply.github.com>
2022-07-14 21:24:52 -0500

    point of experimentation with deck
    trying to draw a card on the screen

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Justin",
+    Accumulated: 0
+}
+
+let cards = []
+const SUITS = ["♠", "♣", "♥", "♦"]
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEL = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
 
+playerEl.textContent = player.name + ": $" + player.Accumulated
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
@@ -20,31 +26,44 @@ function getRandomCard() {
         return randomNumber
     }
 }
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
+
 function renderGame() {
+    let randomType = Math.floor( Math.random()*3 ) + 1
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
+        cardsEl.textContent += cards[i] + SUITS[i]+ " "
     }
-    sumEL.textContent = "Sum: " + sum
+
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
+        player.Accumulated += 1000
         hasBlackJack = true
     } else {
         message = "You're out of the game!"
         isAlive = false
     }
     messageEl.textContent = message
+    playerEl.textContent = player.name + ": $" + player.Accumulated
 }
 
+
 function newCard() {
-    console.log("holi")
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    startGame()
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
 }
